3f7998177fb9b62d1cb25f5172f0d6a2b19c5dc4
mahmudulabin@gmail.com <mahmudulabin@gmail.com>
2026-02-23 19:57:01 +0600

    util a aschi

--- src/main/java/com/example/demo1/app/util/Constants.java
+++ src/main/java/com/example/demo1/app/util/Constants.java
@@ -1,5 +1,5 @@
 package com.example.demo1.app.util;
-
+//this class a project ar global constant values rakha hoi karon hardcoded string avoid korte, same value bar bar likhte na hoy, centralized configuration rakhte, maintain o modify kora easy korte
 public final class Constants {
     private Constants() {}
 
